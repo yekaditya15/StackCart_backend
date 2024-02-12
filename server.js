f5d95bdad5d503be28b46cd2d1b2a3d4aa9e8f38
Yekaditya Devadi <yekaditya33@gmail.com>
2024-02-12 13:49:26 +0530

    authorizatonsetup

--- server.js
+++ server.js
@@ -3,19 +3,24 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoute.js";
 
 const app = express();
-//configure env
+// Configure env
 dotenv.config();
 
+// Connect to the database
 connectDB();
 
-//middleware
+// Middleware
 app.use(express.json());
-app.use(morgan("default"));
+app.use(morgan("dev")); // Use "dev" format for morgan logging
+
+// Routes
+app.use("/api/v1/auth", authRoute);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
+  res.send("<h1>Welcome to the ecommerce app</h1>");
 });
 
 const PORT = process.env.PORT || 8080;
